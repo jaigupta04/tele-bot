2d2d786eee9674fbec0ce7c6290660a01cbab935
Jai Gupta <jaiwork04@gmail.com>
2025-03-10 19:20:30 +0530

    key changes

--- bot.js
+++ bot.js
@@ -4,7 +4,10 @@ const admin = require('firebase-admin');
 const moment = require('moment');
 
 // Initialize Firebase
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf-8")
+);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
--- scheduler.js
+++ scheduler.js
@@ -4,7 +4,9 @@ const admin = require('firebase-admin');
 const moment = require('moment');
 
 // Initialize Firebase
-const serviceAccount = require("./serviceAccountKey.json"); // Directly import the file
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf-8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
